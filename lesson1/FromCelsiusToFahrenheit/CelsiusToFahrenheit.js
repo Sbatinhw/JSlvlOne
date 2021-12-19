f103eed9e9fa0f6b06a6174509145fe448d1e360
Сергей <saygon707@gmail.com>
2021-12-19 19:22:05 +0300

    Добавьте файлы проекта.

---
+++ lesson1/FromCelsiusToFahrenheit/CelsiusToFahrenheit.js
@@ -0,0 +1,6 @@
+"use strict";
+
+let celsius = prompt("Введите значение в градусах по Цельсию:");
+let fahrenheit = (9 / 5) * celsius + 32;
+
+alert("Температура в градусах по Фаренгейту: " + fahrenheit);
